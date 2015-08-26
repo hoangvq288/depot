335eeaac4e3ed4ec5c7eb2ef35770d7650ac4e28
quochoang <hoangvq288@gmail.com>
2015-08-26 17:07:18 +0700

    task f

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,8 @@ class LineItemsController < ApplicationController
     @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart}
+        format.js {@current_item = @line_item}
+        format.html { redirect_to store_url}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
